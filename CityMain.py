a1dfc90fdb8d00c749259bc7b70decbc3d5f9e6c
dman76 <75217549+dman76@users.noreply.github.com>
2020-12-26 15:51:56 +0000

    Add files via upload
    Guardian Statues configuration implemented

--- CityMain.py
+++ CityMain.py
@@ -4,6 +4,7 @@
 import pygame as pg
 from pygame.locals import *
 import numpy as np
+#import MonsterList
 
 #output file inits
 
@@ -16,6 +17,11 @@ tavernFile = open('E8DE-E8F5_inns_data.asm', 'w')
 tavernFile.write('inns_data:')
 tavernFile.write('\n')
 tavernFile.close()
+
+statueFile = open('E856-E87D_guardians.asm', 'w')
+statueFile.write('___table_70:')
+statueFile.write('\n')
+statueFile.close()
 
 def main():
     """ Main Program """
@@ -105,7 +111,7 @@ def diag(xcount, spaceToFill, spaceLeft, addCount, last):
 def writeTavern():
     tavernFile = open('E8DE-E8F5_inns_data.asm', 'a')
     for t in range (1, 8):
-        tavernFile.write('		db  ')
+        tavernFile.write('db  ')
         tavernFile.write(str(INNS[t][2])+', ')
         tavernFile.write(str(INNS[t][3])+', ')
         tavernFile.write(str(INNS[t][1]))
@@ -114,8 +120,33 @@ def writeTavern():
     tavernFile.write('\n')
     tavernFile.write('default_inn:')
     tavernFile.write('\n')
-    tavernFile.write('		db 0FFh,0FFh,0')
+    tavernFile.write('db 0FFh,0FFh,0')
     tavernFile.close()
+
+def writeStatues():
+    statueFile = open('E856-E87D_guardians.asm', 'a')
+    for s in range (1, 11):
+        statueFile.write('\n')
+        statueFile.write('          db    ')
+        statueFile.write(str(STATUES[s][7]))
+        
+    statueFile.write('\n')   
+    statueFile.write('\n')
+    statueFile.write('GUARDIANS:')
+    for s in range (1, 11):
+        statueFile.write('\n')
+        statueFile.write('          db  ')
+        statueFile.write(str(STATUES[s][1]))
+        statueFile.write('h')
+
+    statueFile.write('\n')
+    statueFile.write('\n')
+    statueFile.write('GUARDIAN_COORDS:')
+    for s in range (1, 11):
+        statueFile.write('\n')
+        statueFile.write('          db  ')
+        statueFile.write(str(STATUES[s][3]+','+STATUES[s][4]))
+    statueFile.close()
 
 
 def pack_2():
@@ -241,7 +272,7 @@ def pack_2():
 def find_Guild():
     guildCount = 0
     gfile = open('constants_guild.asm', 'w')
-    gfile.write('GUILD_COORDS	EQU ')
+    gfile.write('GUILD_COORDSEQU ')
     for z in range(900):
         if CityOut[z] == "   9":
             if guildCount == 1:
@@ -321,6 +352,125 @@ def configureTavern(x):
         t0Text = font.render("Tavern Updated", True, RED, BLACK)
         screen.blit(t0Text, (1010, 305))
         pg.display.update()
+
+def configureStatue(x):
+
+        print(x)
+        doneStatue = False
+        statueValue = 1
+        monsterValue = 1
+        direction = 0
+        text = font.render(STATUES[statueValue][0], True, RED, BLACK)
+        blankBox(2)
+        blankBox(4)
+        configText = font.render("Configure: "+str(STATUES[statueValue][0]), True, RED, BLACK)
+        screen.blit(configText, (1010, 256))
+
+        t0Text = font.render("Current Coords:", True, RED, BLACK)
+        screen.blit(t0Text, (1010, 305))
+
+        for t in range (1, 11):
+            sText = font.render('Statue: '+str(t), True, RED, BLACK)
+            if STATUES[t][2] == '00':
+                tText = font.render("Unassigned", True, RED, BLACK)
+            else:
+                stat = STATUES[t][2]
+                tText = font.render(str(MONSTERS[stat][0]), True, RED, BLACK)
+            if (STATUES[t][3] == '00' and STATUES[t][4] == '00'):
+                tCoords = font.render("Unassigned", True, RED, BLACK)
+            else:
+                tCoords = font.render("N: "+str(STATUES[t][5])+ "  E: "+str(STATUES[t][6])+ "  Faces: "+ str(STATUES[t][8]), True, RED, BLACK)
+            screen.blit(sText, (1002, (280+(t*60))))
+            screen.blit(tText, (1002, (300+(t*60))))
+            screen.blit(tCoords, (1010,(320+(t*60))))
+        
+        while not doneStatue:
+            for event in pg.event.get():
+                if event.type == pg.KEYDOWN:
+                   
+                    if event.key == pg.K_z:
+                        if statueValue == 1:
+                            statueValue = 10
+                            blankBox(2)
+                        else:
+                            statueValue = statueValue - 1
+                    if event.key == pg.K_x:
+                        if statueValue == 10:
+                            statueValue = 1
+                            blankBox(2)
+                        else:
+                            statueValue = statueValue + 1
+                    if event.key == pg.K_LEFT:
+                        if monsterValue == 1:
+                            monsterValue = 127
+                        else:
+                            monsterValue = monsterValue - 1
+                    if event.key == pg.K_DOWN:
+                        if monsterValue < 11:
+                            monsterValue = 127
+                        else:
+                            monsterValue = monsterValue - 10
+                    if event.key == pg.K_RIGHT:
+                        if monsterValue == 127:
+                            monsterValue = 1
+                        else:
+                            monsterValue = monsterValue + 1
+                    if event.key == pg.K_UP:
+                        if monsterValue > 117:
+                            monsterValue = 1
+                        else:
+                            monsterValue = monsterValue + 10        
+                    if event.key == pg.K_j:
+                        if direction == 0:
+                            direction = 4
+                        else:
+                            direction = direction - 1
+                    if event.key == pg.K_l:
+                        if direction == 4:
+                            direction = 0
+                            blankBox(4)
+                        else:
+                            direction = direction + 1      
+                       
+                    if event.key == pg.K_c:
+                        # confirm
+                        #update north coordinate
+                        STATUES[statueValue][1] = MONSTERS[monsterValue][1]
+                        STATUES[statueValue][2] = monsterValue
+                        STATUES[statueValue][3] = dec2hexAll(getNorth(x))
+                        STATUES[statueValue][4] = dec2hexAll(getEast(x))
+                        STATUES[statueValue][5] = getNorth(x)
+                        STATUES[statueValue][6] = getEast(x)
+                        STATUES[statueValue][7] = direction
+                        STATUES[statueValue][8] = DIRECTIONS[direction]
+                        print (STATUES[statueValue][0])
+                        print (STATUES[statueValue][1])
+                        print (STATUES[statueValue][2])
+                        print (STATUES[statueValue][3])
+                        print (STATUES[statueValue][4])
+                        print (STATUES[statueValue][5])
+                        print (STATUES[statueValue][6])
+                        doneStatue = True
+                        
+            
+            blankBox(1)
+            text = font.render(str(MONSTERS[monsterValue][0]), True, RED, BLACK)
+            screen.blit(text, (1020, 157))
+            configText = font.render("Configure: "+str(STATUES[statueValue][0]), True, RED, BLACK)
+            screen.blit(configText, (1010, 256))
+            face = str(DIRECTIONS[direction])
+            directionText = font.render(face, True, RED, BLACK)
+            screen.blit(directionText, (1065, 210))
+            pg.display.update()
+
+        blankBox(2)
+        blankBox(3)
+        blankBox(4)
+        configText = font.render("  City Editor", True, RED, BLACK)
+        screen.blit(configText, (1020, 256))
+        t0Text = font.render("Statue Updated", True, RED, BLACK)
+        screen.blit(t0Text, (1010, 305))
+        pg.display.update()
                         
 def write_Out():
     first3 = 1
@@ -355,8 +505,9 @@ def write_Out():
                 eightCount = eightCount + 2
             else:
                 eightCount = eightCount + 1
-    # Write Tavern Coords
+    # Write Tavern & Statue Coords
     writeTavern()
+    writeStatues()
             
 pg.init()
    
@@ -436,6 +587,9 @@ configText = font.render("  City Editor", True, RED, BLACK)
 screen.blit(configText, (1020, 256))
 
 
+DIRECTIONS = ['U','N','E','S','W']
+
+
 INNS = {
     #Name , output value, N coord HEX, E coord Hex, N coord Dec, E coord Dec
 0:  ["Unnamed Tavern", "0", "00", "00", "00", "00"],
@@ -448,6 +602,151 @@ INNS = {
 7:  [" Drawn Blade", "4Ah", "00", "00", "00", "00"]
 }
 
+STATUES = {
+    #Name , Monster Type (hex), Monster Type Dec, N coord HEX, E coord Hex, N coord Dec, E coord Dec, direction
+0:  ["Statue 0", "00", "00", "00", "00", "00", "00", "0", "U"],
+1:  ["Statue 1", "00", "00", "00", "00", "00", "00", "0", "U"],
+2:  ["Statue 2", "00", "00", "00", "00", "00", "00", "0", "U"],
+3:  ["Statue 3", "00", "00", "00", "00", "00", "00", "0", "U"],
+4:  ["Statue 4", "00", "00", "00", "00", "00", "00", "0", "U"],
+5:  ["Statue 5", "00", "00", "00", "00", "00", "00", "0", "U"],
+6:  ["Statue 6", "00", "00", "00", "00", "00", "00", "0", "U"],
+7:  ["Statue 7", "00", "00", "00", "00", "00", "00", "0", "U"],
+8:  ["Statue 8", "00", "00", "00", "00", "00", "00", "0", "U"],
+9:  ["Statue 9", "00", "00", "00", "00", "00", "00", "0", "U"],
+10:  ["Statue 10", "00", "00", "00", "00", "00", "00", "0", "U"]
+}
+
+MONSTERS = {
+0:["Monster","00"],
+1:["Kobold","01"],
+2:["Hobbit","02"],
+3:["Gnome","03"],
+4:["Dwarf","04"],
+5:["Thief","05"],
+6:["Hobgoblin","06"],
+7:["Conjurer","07"],
+8:["Magician","08"],
+9:["Orc","09"],
+10:["Skeleton","0A"],
+11:["Nomad","0B"],
+12:["Spider","0C"],
+13:["Mad Dog","0D"],
+14:["Barbarian","0E"],
+15:["Mercenary","0F"],
+16:["Wolf","10"],
+17:["Jade Monk","11"],
+18:["Half Orc","12"],
+19:["Swordsman","13"],
+20:["Zombie","14"],
+21:["Conjurer","15"],
+22:["Magician","16"],
+23:["Sorcerer","17"],
+24:["Wizard","18"],
+25:["Samurai","19"],
+26:["Black Widow","1A"],
+27:["Assassin","1B"],
+28:["Werewolf","1C"],
+29:["Ogre","1D"],
+30:["Wight","1E"],
+31:["Statue","1F"],
+32:["Bladesman","20"],
+33:["Goblin Lord","21"],
+34:["Master Thief","22"],
+35:["Conjurer","23"],
+36:["Magician","24"],
+37:["Sorcerer","25"],
+38:["Wizard","26"],
+39:["Ninja","27"],
+40:["Spinner","28"],
+41:["Scarlet Monk","29"],
+42:["Doppleganger","2A"],
+43:["Stone Giant","2B"],
+44:["Ogre Magician","2C"],
+45:["Jackalwere","2D"],
+46:["Stone Elemental","2E"],
+47:["Blue Dragon","2F"],
+48:["Seeker","30"],
+49:["Dwarf King","31"],
+50:["Samurai Lord","32"],
+51:["Ghoul","33"],
+52:["Conjurer","34"],
+53:["Magician","35"],
+54:["Sorcerer","36"],
+55:["Wizard","37"],
+56:["Azure Monk","38"],
+57:["Weretiger","39"],
+58:["Hydra","3A"],
+59:["Green Dragon","3B"],
+60:["Wraith","3C"],
+61:["Lurker","3D"],
+62:["Fire Giant","3E"],
+63:["Copper Dragon","3F"],
+64:["  Ivory Monk","40"],
+65:["   Shadow","41"],
+66:[" Berserker","42"],
+67:[" Conjurer","43"],
+68:[" Magician","44"],
+69:[" Sorcerer","45"],
+70:["  Wizard","46"],
+71:["White Dragon","47"],
+72:[" Ice Giant","48"],
+73:["  Eye Spy","49"],
+74:[" Ogre Lord","4A"],
+75:["Body Snatcher","4B"],
+76:["   Xorn","4C"],
+77:["  Phantom","4D"],
+78:[" Lesser Demon","4E"],
+79:["   Fred","4F"],
+80:[" Conjurer","50"],
+81:[" Magician","51"],
+82:[" Sorcerer","52"],
+83:["  Wizard","53"],
+84:["Master Ninja","54"],
+85:["  War Giant","55"],
+86:["Warrior Elite","56"],
+87:["Bone Crusher","57"],
+88:["   Ghost","58"],
+89:[" Grey Dragon","59"],
+90:["  Basilisk","5A"],
+91:["  Evil Eye","5B"],
+92:["   Mimic","5C"],
+93:["   Golem","5D"],
+94:["  Vampire","5E"],
+95:["   Demon","5F"],
+96:[" Bandersnatch","60"],
+97:[" Maze Dweller","61"],
+98:["   Mongo","62"],
+99:["Mangar Guard","63"],
+100:["   Gimp","64"],
+101:[" Red Dragon","65"],
+102:["   Titan","66"],
+103:["Master Conjurer","67"],
+104:["Master Magician","68"],
+105:["Master Sorcerer","69"],
+106:[" Mind Shadow","6A"],
+107:["  Spectre","6B"],
+108:[" Cloud Giant","6C"],
+109:["  Beholder","6D"],
+110:["Vampire Lord","6E"],
+111:["Greater Demon","6F"],
+112:["Master Wizard","70"],
+113:["  Mad God","71"],
+114:["Maze Master","72"],
+115:["Death Denizen","73"],
+116:["Jabberwock","74"],
+117:["Black Dragon","75"],
+118:["Mangar","76"],
+119:["Crystal Golem","77"],
+120:["Soul Sucker","78"],
+121:["Storm Giant","79"],
+122:["Ancient Enemy","7A"],
+123:["   Balrog","7B"],
+124:["    Lich","7C"],
+125:["  Archmage","7D"],
+126:["  Demon Lord","7E"],
+127:["   Old Man","7F"]
+}
 
 #Draw initial blank grid
 
@@ -550,6 +849,9 @@ while not done:
                     if CityOut[Pointer] == " 11h":
                         print ('Tavern config')
                         configureTavern(Pointer)
+                    if CityOut[Pointer] == " 60h":
+                        print ('Statue config')
+                        configureStatue(Pointer)
 
                 HOTKEYS = {
                     pg.K_0: 0,
